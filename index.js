c4dbe188595f0194f0747121835de9207e823ddd
gabriela conde <gabiconde97@gmail.com>
2019-08-14 17:51:30 -0300

    Adding middlewares

--- index.js
+++ index.js
@@ -6,75 +6,87 @@ server.use(express.json())
 const projects = [{
   id: "1",
   title: "Gabi",
-  tasks:[]
+  tasks: []
 }];
-//Middlewares
 
-//Rotas
-server.post('/projects',(req, res) => {
-  const {id, title} = req.body
-  projects.push({ 
-  id,
-  title,
-  tasks:[]
+let numberOfRequests = 0;
+
+//Middlewares
+server.use((req, res, next) => {
+  numberOfRequests++;
+  console.log(`Request number: ${numberOfRequests}`)
+  return next();
+})
+
+function idValidation(req, res, next) {
+  const {
+    id
+  } = req.params;
+
+  //const project = projects.find(p => p.id == id);
+  const index = projects.map(p => {
+    return p['id']
+  }).indexOf(id)
+
+  if (index != -1) {
+    req.index = index;
+    return next()
+  } else {
+    return res.status(400).json({
+      error: 'ID Project not found'
+    })
+  }
+}
+
+
+//Routes
+server.post('/projects', (req, res) => {
+  const {
+    id,
+    title
+  } = req.body
+  projects.push({
+    id,
+    title,
+    tasks: []
   })
   return res.json(projects)
 })
 
 server.get('/projects', (req, res) => {
-  return res.json({projects})
+  return res.json({
+    projects
+  })
 })
 
-server.put('/projects/:id', (req, res) => {
-  const { id } = req.params
-  const {title} = req.body
-  const index = projects.map(p => {
-    return p['id'] }).indexOf(id)
-  if (index != -1){
-    projects[index].title =title;
-    return res.json({"message":`Título alterado para ${title}`})
-  }
-  else{
-    return res.status(400).json({
-      error: 'ID Project not found'
-    })
-  }
-  
+server.put('/projects/:id', idValidation, (req, res) => {
+  const {
+    title
+  } = req.body
+  projects[req.index].title = title;
+  return res.json({
+    "message": `Title has been altered to ${title}`
+  })
+
 })
 
-server.delete('/projects/:id', (req, res) => {
-  const { id } = req.params
-  const index = projects.map(p => {
-    return p['id'] 
-  }).indexOf(id)
-
-  if (index != -1){
-    projects.splice(index,1)
-    return res.json({"message":`Projeto deletado`})
-  }
-  else{
-    return res.status(400).json({
-      error: 'ID Project not found'
-    })
-  }
+server.delete('/projects/:id', idValidation, (req, res) => {
+  projects.splice(req.index, 1)
+  return res.json({
+    "message": `Project deleted`
+  })
 })
 
-server.post('/projects/:id/tasks', (req, res) => {
-  const {id} = req.params;
-  const {title} = req.body;
+server.post('/projects/:id/tasks', idValidation, (req, res) => {
+  const {
+    title
+  } = req.body;
 
-  const index = projects.map(p =>{
-    return p['id']
-  }).indexOf(id)
+  projects[req.index].tasks.push(title)
+  return res.json({
+    message: projects
+  })
 
-  if (index != -1){
-    projects[index].tasks.push(title)
-    return res.json({
-      message:projects
-    })
-  } else {
-    return res.status(400).json({error : 'Project not found'})
-  }
 })
 
 server.listen(3000)
